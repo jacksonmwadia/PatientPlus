d4a02b583f0cdb2244274ae34be6622897430e81
jacksonmwadia <mwadiajackson@gmail.com>
2024-11-07 11:21:58 +0300

    working on static files

---
+++ patientPlus/patientapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return  render(request, 'about_us.html')
+
+def contact(request):
+    return  render(request, 'contact.html')
